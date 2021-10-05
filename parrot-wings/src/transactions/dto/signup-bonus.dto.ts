a3581ef6ac97c109f9d82bb9a27247f9334d42a3
Amal <maroon775@gmail.com>
2021-10-05 13:18:45 +0500

    PW - fix issues

--- parrot-wings/src/transactions/dto/signup-bonus.dto.ts
+++ parrot-wings/src/transactions/dto/signup-bonus.dto.ts
@@ -1,7 +1,10 @@
 import { User } from '../../users/interfaces/user';
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsObject } from 'class-validator';
 
 export class SignupBonusDto {
   @ApiProperty()
+  @IsObject()
+  @IsNotEmpty()
   user: User;
 }
